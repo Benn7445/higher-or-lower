d23eb19d50a414e6f39969abab63e777892f8e5a
Dennis Matheuwezen <dennis.matheuwezen@student.ap.be>
2022-12-31 04:26:54 +0100

    removal deprecated

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, DarkTheme } from "@react-navigation/native";
 import HomeScreen from "./src/pages/HomeScreen";
-import LeaderboardScreen from "./src/pages/LeaderboardScreen";
+
 import AntDesign from "react-native-vector-icons/AntDesign";
 
 import Higher from "./src/pages/gamecode";
@@ -29,14 +29,15 @@ export const App = () => {
             tabBarInactiveTintColor: "#808080",
           }}
         />
+
         <Tab.Screen
-          name="Leaderboard"
-          component={LeaderboardScreen}
+          name="gameV2"
+          component={Higher}
           options={{
             tabBarIcon: (active) => {
               return (
                 <AntDesign
-                  name="Trophy"
+                  name="play"
                   color={active.focused ? "#008080" : "#808080"}
                   size={20}
                 />
@@ -46,23 +47,6 @@ export const App = () => {
             tabBarInactiveTintColor: "#808080",
           }}
         />
-        <Tab.Screen 
-        name="gameV2" 
-        component={Higher}
-        options={{
-          tabBarIcon: (active) => {
-            return (
-              <AntDesign
-                name="play"
-                color={active.focused ? "#008080" : "#808080"}
-                size={20}
-              />
-            );
-          },
-          tabBarActiveTintColor: "#008080",
-          tabBarInactiveTintColor: "#808080",
-        }}
-         />
       </Tab.Navigator>
     </NavigationContainer>
   );
